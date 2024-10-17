ff8ed0c0254007df9a0a42f164b945f34d0151ae
Saihritesh <143974228+Saihritesh@users.noreply.github.com>
2024-10-17 15:18:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+dotenv.config();
+import * as fs from "fs";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI("AIzaSyAGkcYonGKVih4eFUmlpWIH6zLfQXDlorA");
+
+function fileToGenerativePart(path, mimeType) {
+    return {
+        inlineData: {
+            data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+            mimeType,
+        },
+    };
+}
+
+async function run() {
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    const prompt =
+        "I need you to guess what type of infection";
+
+    const imageParts = [
+        fileToGenerativePart("ringworm.jpg", "image/jpeg")
+    ];
+
+    const result = await model.generateContent([prompt, ...imageParts]);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+}
+
+run();
